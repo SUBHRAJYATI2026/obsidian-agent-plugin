e2e8291b17a68039e66f216d1badc9c2fe0ba2de
SUBHRAJYOTI MANDAL <subhrajyotimandal1216@gmail.com>
2026-03-28 21:04:14 +0530

    added routes

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-
-db_store: dict[int, str] = {}
-counter: int = 0
+from src.routes import router
 
 app = FastAPI()
+app.include_router(router=router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -13,22 +11,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-class Item(BaseModel):
-    """Schema for the prompt to the ai agent."""
-
-    prompt: str
-
-
-@app.post("/generate")
-def generate(request: Item):
-    global counter
-    counter += 1
-    db_store[counter] = request.prompt
-    return {"response": f"Prompt received: {request.prompt}"}
-
-
-@app.get("/prompts")
-def get_prompts():
-    return db_store
