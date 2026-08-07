7918ec7cc2e580193ae31f4ca6e1e78536776ae5
SUBHRAJYOTI MANDAL <subhrajyotimandal1216@gmail.com>
2026-08-07 13:32:17 +0530

    removed plugin settings and notes tool

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,13 @@ import { createRoot, Root } from "react-dom/client";
 import { StrictMode } from "react";
 import React from "react";
 import FloatingWidget from "./FloatingWidget";
-import { AIPluginSettings, DEFAULT_SETTINGS, AISettingTab } from "./settings";
 
 export default class AIPlugin extends Plugin {
   private root: Root | null = null;
   private container: HTMLElement | null = null;
-  settings: AIPluginSettings = DEFAULT_SETTINGS;
 
   async onload() {
     // Create a div and append to document.body
-    await this.loadSettings();
-    this.addSettingTab(new AISettingTab(this.app, this));
     this.container = document.createElement("div");
     this.container.id = "my-plugin-root";
     document.body.appendChild(this.container);
@@ -31,17 +27,5 @@ export default class AIPlugin extends Plugin {
     // Clean up on plugin disable
     this.root?.unmount();
     this.container?.remove();
-  }
-
-  async loadSettings() {
-    this.settings = Object.assign(
-      {},
-      DEFAULT_SETTINGS,
-      (await this.loadData()) as Partial<AIPluginSettings>,
-    );
-  }
-
-  async saveSettings() {
-    await this.saveData(this.settings);
   }
 }
